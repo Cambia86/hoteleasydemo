d065d70d446cf62368044a0f7c02811f98225e40
Alessandro <alessandrocambiaghi@gmail.com>
2017-07-07 16:38:05 +0200

    add directive sidebar

--- public/app/directive/sidebarButton.js
+++ public/app/directive/sidebarButton.js
@@ -1,3 +1,21 @@
 /**
  * Created by aless on 06/07/2017.
  */
+(function () {
+    'use strict';
+
+    angular
+        .module('hotelEasy')
+        .directive('sidebarbutton', sidebarbutton);
+
+    sidebarbutton.$inject = ['appVersion'];
+    function sidebarbutton(appVersion) {
+        return {
+            restrict: 'E',
+            scope: {
+                quest: '=quest'
+            },
+            templateUrl: 'app/views/directive/sidebarButton.html?v=' + appVersion
+        };
+    }
+})();
